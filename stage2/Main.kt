7758a50a3cc23ae2c00f951558e307a27199567d
misuy <mperedriy@bk.ru>
2022-01-02 04:32:21 +0300

    stage3

--- stage2/Main.kt
+++
@@ -1,7 +0,0 @@
-package addLabStage2;
-
-fun main(args: Array<String>)
-{
-    var app = App();
-    app.run();
-}
---
+++ stage3/Main.kt
@@ -0,0 +1,7 @@
+package addLabStage3;
+
+fun main(args: Array<String>)
+{
+    var app = App();
+    app.run();
+}
